8c16ef107e58cdd86aeebc6fb3840d4095e42437
dufnill <davide.fiorini.davide@gmail.com>
2024-02-22 14:24:30 +0100

    First commit from netbeans

---
+++ src/javaapplicationbj/JavaApplicationBJ.java
@@ -0,0 +1,22 @@
+package javaapplicationbj;
+
+    /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author dufnill
+ */
+public class JavaApplicationBJ {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        BJWindow window = new BJWindow(); 
+        window.setVisible(true);
+    }
+    
+}
